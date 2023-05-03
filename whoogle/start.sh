5bb02f958d76438c4e27929dea03efd27ccf658c
PardesiCat <contact@pardesicat.xyz>
2023-05-03 18:38:57 +0530

    shifted whoogle to docker compose

--- whoogle/start.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-#ofc these scipits are writtenby me 
-echo -e "Starting whoogle container"
-sleep 2
-
-docker run --detach --network=npm_default --restart=always --env-file=whoogle.env  --name whoogle-search benbusby/whoogle-search:latest
-
