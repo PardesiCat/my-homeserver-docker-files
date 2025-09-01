458b75d40f83b8c9f504a1e96cfcd78cc484bdf1
PardesiCat <contact@pardesicat.xyz>
2025-09-02 00:46:05 +0530

    fix + update the matrix server

--- matrix.org/setup_matrix_server.sh
+++ matrix.org/setup_matrix_server.sh
@@ -1,16 +1,20 @@
-#written by Pardesi_Cat
+#!/bin/bash
+# written by Pardesi_Cat
 
-echo -e "\e[1;32m Type your server domain\e[0m"
-read domain
+echo -e "\e[1;32mType your server domain:\e[0m"
+read -r domain
 
 sleep 2
-echo -e "starting the 1st setup"
-mkdir synapse-data
+echo -e "\e[1;34mStarting the first setup...\e[0m"
+
+# Create data directory if it doesn't exist
+mkdir -p synapse-data
 sleep 2
 
+# Run docker with correct variable usage
 docker run -it --rm \
-    --mount type=bind,src=$(pwd)/synapse-data,target=/data \
-    -e SYNAPSE_SERVER_NAME=${DOMAIN} \
+    --mount type=bind,src="$(pwd)/synapse-data",target=/data \
+    -e SYNAPSE_SERVER_NAME="${domain}" \
     --user=0:0 \
     -e SYNAPSE_REPORT_STATS=yes \
     matrixdotorg/synapse:latest generate
